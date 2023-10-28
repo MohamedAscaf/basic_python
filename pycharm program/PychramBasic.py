328656f5c8ef798e5fea50bd098a0cfda0677796
MohamedAscaf <145448363+MohamedAscaf@users.noreply.github.com>
2023-10-28 16:20:38 +0530

    Add files via upload

---
+++ pycharm program/PychramBasic.py
@@ -0,0 +1,24 @@
+# f=open("my file.txt","a")
+# f.write("example of file handling")
+# f.close()
+
+#open and read the file after the appending
+
+# f = open ("my file.txt","r")
+# print(f.read())
+
+#create a new file
+
+# f = open("mypycharm.txt" , "x")
+
+# remove the file
+# import os
+# if os.path.exists("mypycharm.txt"):
+#    os.remove("mypycharm.txt")
+# else:
+#     print ("the file does not exist")
+
+#remove the folder
+
+import os
+os.rmdir("AB")
